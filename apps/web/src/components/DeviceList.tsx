45a90ec8897735a4d6d5f07a2c41e8815d4e1aef
JMontes <damon.1986@icloud.com>
2025-10-24 15:30:22 -0500

    Update DeviceList.tsx

--- apps/web/src/components/DeviceList.tsx
+++ apps/web/src/components/DeviceList.tsx
@@ -138,7 +138,7 @@ const DeviceList: React.FC = () => {
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredDevices.map((device) => (
-            
+            <a
               key={device.id}
               href={`/inventory/${device.id}`}
               className="card hover:shadow-lg transition-shadow cursor-pointer"
