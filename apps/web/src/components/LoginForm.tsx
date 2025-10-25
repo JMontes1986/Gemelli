15184bcd2ba79c64c963a7cc307d9d750eb61947
JMontes <damon.1986@icloud.com>
2025-10-24 20:54:22 -0500

    Update LoginForm.tsx

--- apps/web/src/components/LoginForm.tsx
+++ apps/web/src/components/LoginForm.tsx
@@ -120,7 +120,7 @@ const LoginForm: React.FC = () => {
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <User className="h-5 w-5 text-gray-400" />
+                  <UserRound className="h-5 w-5 text-gray-400" />
                 </div>
                 <input
                   id="fullName"
