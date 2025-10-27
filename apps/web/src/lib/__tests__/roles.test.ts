039fd18cde10dcfafc8fd813d021f39e1be675ec
JMontes <damon.1986@icloud.com>
2025-10-27 08:55:08 -0500

    Update roles.test.ts

--- apps/web/src/lib/__tests__/roles.test.ts
+++ apps/web/src/lib/__tests__/roles.test.ts
@@ -6,6 +6,8 @@ assert.equal(normalizeRole('ti'), 'TI');
 assert.equal(normalizeRole('docente'), 'DOCENTE');
 assert.equal(normalizeRole('lider ti'), 'LIDER_TI');
 assert.equal(normalizeRole('lider-ti'), 'LIDER_TI');
+assert.equal(normalizeRole('Líder TI'), 'LIDER_TI');
+assert.equal(normalizeRole('líder   ti'), 'LIDER_TI');
 assert.equal(normalizeRole(' DIRECTOR '), 'DIRECTOR');
 assert.equal(normalizeRole('ADMINISTRATIVO'), 'ADMINISTRATIVO');
 
